9a3a775584caf4ee4590cc0d68eea53ae5b6a196
Cata Quarleri <91096792+CataQuarleri@users.noreply.github.com>
2022-07-17 19:00:46 -0600

    Feature search bar (#3)
    * add frontend structure, banner and searchBar
    
    * fixed flexbox issues, create background and cards

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './Views/Home'
+import ThemeProvider from 'react-bootstrap/ThemeProvider'
 
-function App() {
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+//     <ThemeProvider
+//   breakpoints={['xl', 'lg', 'md', 'sm']}
+//   minBreakpoint="sm"
+// >
+      <Home/>
+    // </ThemeProvider>
   );
 }
 
---
+++ frontend/src/Components/Banner.js
@@ -0,0 +1,38 @@
+import React, { useRef } from 'react'
+import tgl_logo from '../assets/TGLAB_logo.svg'
+import SearchBar from './SearchBar'
+
+function Banner() {
+    const searchInput = useRef()
+
+    function handleInput() {
+        console.log(searchInput.current.value)
+    }
+  return (
+
+    <div
+    className='p-5 text-center bg-image'
+    style={{
+        backgroundImage:
+            "url('https://edu.teaminternational.com/wp-content/uploads/2020/10/tpl_hero.jpg')",
+        height: 400,
+    }}>
+    <div
+        className='mask'
+        style={{
+            backgroundColor: 'rgba(0, 0, 0, 0.4)',
+        }}>
+        <div className='d-flex p-4 justify-content-center flex-column align-items-center'
+         >
+                <img className="img-fluid min-width" src={tgl_logo}/>
+                <h1 className='mb-3 text-white'>
+                    Movie Search Engine
+                </h1>
+             <SearchBar/>
+        </div>
+    </div>
+</div>
+  )
+}
+
+export default Banner
