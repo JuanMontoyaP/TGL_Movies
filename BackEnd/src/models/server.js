898ba9e2538faad7911c38d4adb59c89c4e68040
Juan Pablo M <jpmontoya19@gmail.com>
2022-07-18 22:33:12 -0500

    Add the end point for show popular movies

--- BackEnd/src/models/server.js
+++ BackEnd/src/models/server.js
@@ -1,34 +1,30 @@
-const express = require('express')
+const express = require("express");
 
 class Server {
+  constructor() {
+    this.app = express();
+    this.usuariosPath = "/users";
+    this.moviesPath = "/movies";
 
-    constructor() {
-        this.app = express()
-        this.usuariosPath = '/users'
-        this.moviesPath = '/movies'
+    //Middlewares
+    this.middlewares();
 
-        //Middlewares
-        this.middlewares()
+    // Rutas de la app
+    this.routes();
+  }
 
-        // Rutas de la app
-        this.routes()
-    }
+  middlewares() {}
 
-    middlewares() {
-        
-    }
+  routes() {
+    this.app.use(this.usuariosPath, require("../routes/users"));
+    this.app.use(this.moviesPath, require("../routes/movies"));
+  }
 
-    routes() {
-
-        this.app.use(this.usuariosPath, require('../routes/users'))
-        this.app.use(this.moviesPath, require('../routes/movies'))
-    }
-
-    listen() {
-        this.app.listen(8080, () => {
-            console.log('Server running in port 8080')
-        })
-    }
+  listen() {
+    this.app.listen(8080, () => {
+      console.log("Server running in port 8080");
+    });
+  }
 }
 
-module.exports = Server
+module.exports = Server;
