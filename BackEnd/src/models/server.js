ab36753e8ce49db740164247db5c16d9165c2f0b
Juan Pablo M <jpmontoya19@gmail.com>
2022-07-29 18:48:12 -0500

    Fix cors library

--- BackEnd/src/models/server.js
+++ BackEnd/src/models/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require('cors')
 const { dbConnection } = require("../database/config");
-const cors = require('cors')
 
 class Server {
     constructor() {
